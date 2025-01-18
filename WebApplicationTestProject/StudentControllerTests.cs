7b69555871ad784879c03974c6049ef7b0533d29
Dawid Ryskala <dawid.ryskala@microsoft.wsei.edu.pl>
2025-01-18 10:48:57 +0100

    Lab-05-zad1

--- WebApplicationTestProject/StudentControllerTests.cs
+++ WebApplicationTestProject/StudentControllerTests.cs
@@ -3,215 +3,138 @@ using Moq;
 using WebApplication1.Controllers;
 using WebApplication1.Models;
 using WebApplication1.Services;
+using NUnit.Framework;
+using System.Diagnostics.CodeAnalysis;
 
 namespace WebApplicationTestProject
 {
+    [SuppressMessage("Usage", "NUnit1032:An IDisposable field/property should be Disposed in a TearDown method", Justification = "Mocks do not require Dispose.")]
     public class StudentControllerTests
     {
-        List<StudentViewModel> students;
-        Mock<IStudentService> studentServiceMock;
+        private List<StudentViewModel> students;
+        private Mock<IStudentService> studentServiceMock;
+        private StudentController studentController;
 
         [SetUp]
         public void Setup()
         {
-            // fill students model mock
-            students = new List<StudentViewModel>();
-            students.Add(new StudentViewModel() { Id = 1, Name = "Asterix", IndexNumber = "000001" });
-            students.Add(new StudentViewModel() { Id = 2, Name = "Obelix", IndexNumber = "000002" });
+            students = new List<StudentViewModel>
+            {
+                new StudentViewModel { Id = 1, Name = "Asterix", IndexNumber = "000001" },
+                new StudentViewModel { Id = 2, Name = "Obelix", IndexNumber = "000002" }
+            };
 
-            // create service mock
             studentServiceMock = new Mock<IStudentService>();
+            studentController = new StudentController(studentServiceMock.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Mocks nie wymagają Dispose(), więc pozostaje pusta.
         }
 
         [Test]
-        public void TestIndexAction()
+        public void TestDetailsAction_ValidId_ReturnsView()
         {
-            // Arrange
-            this.studentServiceMock.Setup(m => m.FindAll()).Returns(students);
-            var studentController = new StudentController(studentServiceMock.Object);
+            var student = students.First();
+            studentServiceMock.Setup(m => m.FindById(1)).Returns(student);
 
-            // Act
-            var result = studentController.Index();
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.True(result is ViewResult);
-            var viewResult = result as ViewResult;
-            Assert.IsTrue(string.IsNullOrEmpty(viewResult.ViewName) || viewResult.ViewName == "Index");
-            Assert.IsNotNull(viewResult.Model);
-            Assert.IsTrue(viewResult.Model is List<StudentViewModel>);
-            var studentsModel = viewResult.Model as List<StudentViewModel>;
-            Assert.That(studentsModel, Has.Count.EqualTo(2));
-        }
-
-        // Test akcjia Create
-
-        [Test]
-        public void TestCreatePost_ValidModel_RedirectsToIndex()
-        {
-            // Arrange
-            var newStudent = new StudentViewModel { Id = 3, Name = "New Student", Email = "new@student.com" };
-            this.studentServiceMock.Setup(m => m.Add(It.IsAny<StudentViewModel>()));
-
-            var studentController = new StudentController(studentServiceMock.Object);
-
-            // Act
-            var result = studentController.Create(newStudent);
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<RedirectToActionResult>(result);
-            var redirectResult = result as RedirectToActionResult;
-            Assert.AreEqual("Index", redirectResult.ActionName);
-        }
-
-        [Test]
-        public void TestCreatePost_InvalidModel_ReturnsView()
-        {
-            // Arrange
-            var newStudent = new StudentViewModel { Name = "", Email = "invalid_email" }; // Invalid data
-            var studentController = new StudentController(studentServiceMock.Object);
-            studentController.ModelState.AddModelError("Name", "Name is required");
-
-            // Act
-            var result = studentController.Create(newStudent);
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<ViewResult>(result);
-            var viewResult = result as ViewResult;
-            Assert.AreEqual(newStudent, viewResult.Model);
-        }
-
-        // Test akcji Edit
-
-        [Test]
-        public void TestEditGet_ValidId_ReturnsViewWithModel()
-        {
-            // Arrange
-            var student = new StudentViewModel { Id = 1, Name = "Existing Student", Email = "existing@student.com" };
-            this.studentServiceMock.Setup(m => m.FindById(1)).Returns(student);
-
-            var studentController = new StudentController(studentServiceMock.Object);
-
-            // Act
-            var result = studentController.Edit(1);
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<ViewResult>(result);
-            var viewResult = result as ViewResult;
-            Assert.AreEqual(student, viewResult.Model);
-        }
-
-        [Test]
-        public void TestEditGet_InvalidId_ReturnsNotFound()
-        {
-            // Arrange
-            this.studentServiceMock.Setup(m => m.FindById(2)).Returns((StudentViewModel)null);
-
-            var studentController = new StudentController(studentServiceMock.Object);
-
-            // Act
-            var result = studentController.Edit(2);
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<NotFoundResult>(result);
-        }
-
-        // Test akcjia Details
-
-        [Test]
-        public void TestDetails_ValidId_ReturnsViewWithModel()
-        {
-            // Arrange
-            var student = new StudentViewModel { Id = 1, Name = "Student Details", Email = "details@student.com" };
-            this.studentServiceMock.Setup(m => m.FindById(1)).Returns(student);
-
-            var studentController = new StudentController(studentServiceMock.Object);
-
-            // Act
             var result = studentController.Details(1);
 
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.That(result, Is.Not.Null);
             var viewResult = result as ViewResult;
-            Assert.AreEqual(student, viewResult.Model);
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult!.Model, Is.EqualTo(student));
         }
 
         [Test]
-        public void TestDetails_InvalidId_ReturnsNotFound()
+        public void TestDetailsAction_InvalidId_ReturnsNotFound()
         {
-            // Arrange
-            this.studentServiceMock.Setup(m => m.FindById(3)).Returns((StudentViewModel)null);
+            studentServiceMock.Setup(m => m.FindById(It.IsAny<int>())).Returns((StudentViewModel)null);
 
-            var studentController = new StudentController(studentServiceMock.Object);
+            var result = studentController.Details(999);
 
-            // Act
-            var result = studentController.Details(3);
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<NotFoundResult>(result);
+            Assert.That(result, Is.InstanceOf<NotFoundResult>());
         }
 
-        // Test akcji Delete
+        [Test]
+        public void TestCreateAction_ValidModel_RedirectsToIndex()
+        {
+            var newStudent = new StudentViewModel { Name = "Panoramix", IndexNumber = "000003" };
+            studentServiceMock.Setup(m => m.Add(It.IsAny<StudentViewModel>()));
+
+            var result = studentController.Create(newStudent);
+
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+            var redirectResult = result as RedirectToActionResult;
+            Assert.That(redirectResult, Is.Not.Null);
+            Assert.That(redirectResult!.ActionName, Is.EqualTo("Index"));
+        }
 
         [Test]
-        public void TestDeleteGet_ValidId_ReturnsViewWithModel()
+        public void TestCreateAction_InvalidModel_ReturnsView()
         {
-            // Arrange
-            var student = new StudentViewModel { Id = 1, Name = "Student To Delete", Email = "delete@student.com" };
-            this.studentServiceMock.Setup(m => m.FindById(1)).Returns(student);
+            var invalidStudent = new StudentViewModel { Name = "", IndexNumber = "000" };
+            studentController.ModelState.AddModelError("Name", "Name is required");
 
-            var studentController = new StudentController(studentServiceMock.Object);
+            var result = studentController.Create(invalidStudent);
 
-            // Act
-            var result = studentController.Delete(1);
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<ViewResult>(result);
+            Assert.That(result, Is.InstanceOf<ViewResult>());
             var viewResult = result as ViewResult;
-            Assert.AreEqual(student, viewResult.Model);
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult!.Model, Is.EqualTo(invalidStudent));
         }
 
         [Test]
-        public void TestDeleteGet_InvalidId_ReturnsNotFound()
+        public void TestEditAction_ValidModel_RedirectsToIndex()
         {
-            // Arrange
-            this.studentServiceMock.Setup(m => m.FindById(4)).Returns((StudentViewModel)null);
+            var existingStudent = students.First();
+            studentServiceMock.Setup(m => m.Update(existingStudent));
 
-            var studentController = new StudentController(studentServiceMock.Object);
+            var result = studentController.Edit(existingStudent);
 
-            // Act
-            var result = studentController.Delete(4);
-
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<NotFoundResult>(result);
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
+            var redirectResult = result as RedirectToActionResult;
+            Assert.That(redirectResult, Is.Not.Null);
+            Assert.That(redirectResult!.ActionName, Is.EqualTo("Index"));
         }
 
         [Test]
-        public void TestDeletePost_ValidId_RedirectsToIndex()
+        public void TestEditAction_InvalidModel_ReturnsView()
         {
-            // Arrange
-            this.studentServiceMock.Setup(m => m.Delete(1));
+            var invalidStudent = new StudentViewModel { Id = 1, Name = "", IndexNumber = "000" };
+            studentController.ModelState.AddModelError("Name", "Name is required");
 
-            var studentController = new StudentController(studentServiceMock.Object);
+            var result = studentController.Edit(invalidStudent);
 
-            // Act
+            Assert.That(result, Is.InstanceOf<ViewResult>());
+            var viewResult = result as ViewResult;
+            Assert.That(viewResult, Is.Not.Null);
+            Assert.That(viewResult!.Model, Is.EqualTo(invalidStudent));
+        }
+
+        [Test]
+        public void TestDeleteAction_ValidId_RedirectsToIndex()
+        {
+            studentServiceMock.Setup(m => m.Delete(1));
+
             var result = studentController.DeleteConfirmed(1);
 
-            // Assert
-            Assert.IsNotNull(result);
-            Assert.IsInstanceOf<RedirectToActionResult>(result);
+            Assert.That(result, Is.InstanceOf<RedirectToActionResult>());
             var redirectResult = result as RedirectToActionResult;
-            Assert.AreEqual("Index", redirectResult.ActionName);
+            Assert.That(redirectResult, Is.Not.Null);
+            Assert.That(redirectResult!.ActionName, Is.EqualTo("Index"));
         }
 
+        [Test]
+        public void TestDeleteAction_InvalidId_ReturnsNotFound()
+        {
+            studentServiceMock.Setup(m => m.Delete(It.IsAny<int>()));
 
+            var result = studentController.Delete(999);
+
+            Assert.That(result, Is.InstanceOf<NotFoundResult>());
+        }
     }
 }
